34010eb86a1368e65ed9a6ce8b9d50b88f5a2275
koge <koge.k@dentsudigital.co.jp>
2019-06-12 15:55:03 +0900

    no message

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
+<?php
+echo 'リダイレクト';
+if ($_REQUEST) {
+    foreach ($_REQUEST as $aRow) {
+        echo '<br>';
+        var_dump($aRow);
+    }
+}
+
+?>
 <a href="./Web Push Notification Journey Builder Custom Activity _ Demo Center.htm">Demo</a>
